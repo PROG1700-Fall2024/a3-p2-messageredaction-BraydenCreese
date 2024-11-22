28c9ead7d1ffa13319c6e908023337cd07b93f4a
BraydenCreese <kidson2017@gmail.com>
2024-11-21 21:40:04 -0800

    Done. 1 Day Late

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -2,19 +2,29 @@
 #Description:   Design and write a program that counts and removes all desired letters from 
 #               any user-entered sentence or phrase.
 
-#Student #:     
-#Student Name:  
+#Student #: Brayden Creese
+#Student Name: W0491583
 
-def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+def messageRedaction():
+    while True:
+        phrase = input("Type a phrase (or 'quit' to exit): ")
+        if phrase.lower() == "quit":
+            break
 
+        lettersRedacted = input("Enter letters to redact (comma-separated): ").lower()
+        redactedpPhrase = ""
+        redactedCount = 0
 
+        for char in phrase:
+            if char.lower() in lettersRedacted:
+                redactedpPhrase += "_"
+                redactedCount += 1
+            else:
+                redactedpPhrase += char
 
+        print("Number of letters redacted:", redactedCount)
+        print("Redacted phrase:", redactedpPhrase)
+        print()
 
+messageRedaction()
 
-
-
-
-    # YOUR CODE ENDS HERE
-
-main()
